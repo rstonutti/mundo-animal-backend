08420e07bf68d75b330793ab2c43ab8176b18ee9
Rodrigo <rstonutti@gmail.com>
2021-11-29 09:10:32 -0300

    Mejoras en la carga de imágenes

--- controllers/uploads.controllers.js
+++ controllers/uploads.controllers.js
@@ -2,6 +2,8 @@
 const { request, response } = require('express');
 const { subirImagen } = require('../helpers');
 
+const { v4: uuidv4 } = require('uuid');
+
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config(process.env.CLOUDINARY_URL)
@@ -11,17 +13,25 @@ const ctrlImagen = {};
 ctrlImagen.subir = async (req = request, res = response) => {
 
     const { tempFilePath } = req.files.imagen;
+    
+    const nombre = uuidv4();
 
+    console.log(nombre);
     try {
 
-        const { secure_url } = await cloudinary.uploader.upload(tempFilePath);
+
+        const { secure_url, public_id } = await cloudinary.uploader.upload(tempFilePath, 
+            {resource_type: "image", public_id: `mundo_animal/${nombre}`},
+            function(error, result) {
+                error && console.log(error)});
 
         //const nombre = await subirImagen(req.files, 'img', undefined);
 
 
 
         res.json({
-            secure_url
+            secure_url,
+            public_id
         });
 
     } catch (msg) {
